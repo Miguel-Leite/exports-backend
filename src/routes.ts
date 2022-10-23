491746db5c1d33d60d01304d83b97875dcb153f1
Miguel-Leite <miguelleite200leite@email.com>
2022-10-23 20:50:56 +0100

    add new router

--- src/routes.ts
+++ src/routes.ts
@@ -1,7 +1,9 @@
 import { Router } from 'express';
 import { PrismaClient } from '@prisma/client';
 import { ListGamesController } from './controllers/ListGamesController';
+import { ListAdsForIdGamesController } from './controllers/ListAdsForIdGamesController';
 
 export const router = Router();
 
 router.get('/games', new ListGamesController().handle)
+router.get('/games/:id/ads', new ListAdsForIdGamesController().handle)
