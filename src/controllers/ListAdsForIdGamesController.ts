b23bbe6a9b8a76345764e2f7750ec759acdac272
Miguel-Leite <miguelleite200leite@email.com>
2022-10-23 19:01:27 +0100

    created controller ads

---
+++ src/controllers/ListAdsForIdGamesController.ts
@@ -0,0 +1,19 @@
+import { Request, Response } from "express";
+import { ListAdsForIdGamesService } from "../services/ListAdsForIdGamesService";
+
+
+class ListAdsForIdGamesController {
+
+  async handle(request: Request, response: Response) {
+    const { id } = request.params;
+
+    const listAdsForIdGamesService = new ListAdsForIdGamesService();
+
+    const result = listAdsForIdGamesService.execute(id)
+
+    return response.json(result)
+  }
+
+}
+
+export { ListAdsForIdGamesController }
