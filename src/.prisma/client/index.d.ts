b1f347b5d44bf81b659312f6cb99cdd504527f14
Miguel-Leite <miguelleite200leite@email.com>
2022-10-26 17:00:48 +0100

    remove file and add cors in app

--- src/.prisma/client/index.d.ts
+++
@@ -1,5 +0,0 @@
-declare namespace Prisma {
-  export type AdUncheckedCreateInput = {
-    id?: string;
-  }
-}
--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,12 @@
-import express from "express"
+import express from "express";
+import cors from 'cors';
 import { router } from "./routes";
 
 const app = express();
+
+app.use(cors({
+  origin: "*"
+}))
 
 app.use(express.json())
 
